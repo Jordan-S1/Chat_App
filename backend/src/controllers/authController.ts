0b56e6c6f4d0622208190b95d7b4365d2c11f1dd
Jordan-S1 <jordanshodipo1@gmail.com>
2025-02-22 20:51:14 +0000

    modified files

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,60 @@
 import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
+import bcryptjs from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
 
 export const login = async (req: Request, res: Response) => {};
 export const logout = async (req: Request, res: Response) => {};
-export const signup = async (req: Request, res: Response) => {};
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    // Check if all fields are filled
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: "Please fill in all fields" });
+    }
+    // Check if password and confirmPassword match
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
+    // Check if username already exists
+    const user = await prisma.user.findUnique({ where: { username } });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+    // Hash
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    // Create user
+    // https://avatar-placeholder.iran.liara.run/
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+      },
+    });
+    if (newUser) {
+      // Generate token
+      generateToken(newUser.id, res);
+
+      res.status(201).json({
+        id: newUser.id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error: any) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/authRoute.js";
 import messageRoutes from "./routes/messageRoute.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+
+app.use(express.json()); // for parsing application/json
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
